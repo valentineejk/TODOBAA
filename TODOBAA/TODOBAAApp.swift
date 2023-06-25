144469e7fee60c38963ff61a2b3612aba0b58172
valentineejk <valentineejk@gmail.com>
2023-06-25 19:38:04 +0100

    Initial Commit

---
+++ TODOBAA/TODOBAAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOBAAApp.swift
+//  TODOBAA
+//
+//  Created by Valentineejk on 25/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TODOBAAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
